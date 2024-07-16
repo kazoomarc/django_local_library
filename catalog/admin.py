5c08a3c44f5e4d538456cecbf5dd8315e85b8d63
Joel Mwala <joelmwala7@gmail.com>
2024-07-16 16:13:56 +0200

    customise admin page fo rinline and block views

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,27 +3,45 @@ from django.contrib import admin
 # Register your models here.
 from .models import Author, Genre, Book, BookInstance, Language
 
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+    
 # register book Model using decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "display_genre")
+    inlines = [BooksInstanceInline]
 
-# admin.site.register(Book, BookAdmin)
-
-
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+    
 # define the admin class
+
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-
-# register the admin class with the associated model
-admin.site.register(Author, AuthorAdmin)
+    fields = [('first_name', 'last_name'), ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
 
 admin.site.register(Genre)
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("book", "status", "due_back",  "id")
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+    
 
-# admin.site.register(BookInstance, BookInstanceAdmin)
-
-admin.site.register(Language)
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+    
