4d718f0350caffc606a2f43024b2a9aaba79b61e
Joel Mwala <joelmwala7@gmail.com>
2024-07-19 23:23:09 +0200

    add sessions

--- catalog/views.py
+++ catalog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+
 
 def index(request):
     """View function for home page of site."""
@@ -18,14 +20,28 @@ def index(request):
     num_genres_fiction = Genre.objects.filter(name__icontains = 'fiction').count()
     num_books_thriller = Book.objects.filter(genre__name__icontains = 'thriller').count()
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genres_fiction':num_genres_fiction,
-        'num_books_thriller':num_books_thriller    
+        'num_books_thriller':num_books_thriller,
+        'num_visits': num_visits,  
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 4
+    
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
