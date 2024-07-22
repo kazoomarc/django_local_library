cc0e040993f66c696b397c86ca757a87e24c2476
Joel Mwala <joelmwala7@gmail.com>
2024-07-22 19:01:06 +0200

    everything to add userborrowed books

--- catalog/views.py
+++ catalog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def index(request):
@@ -13,35 +14,49 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
 
     # Available books (status = 'a')
-    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_instances_available = BookInstance.objects.filter(status__exact="a").count()
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
-    num_genres_fiction = Genre.objects.filter(name__icontains = 'fiction').count()
-    num_books_thriller = Book.objects.filter(genre__name__icontains = 'thriller').count()
+    num_genres_fiction = Genre.objects.filter(name__icontains="fiction").count()
+    num_books_thriller = Book.objects.filter(genre__name__icontains="thriller").count()
 
-    num_visits = request.session.get('num_visits', 0)
-    request.session['num_visits'] = num_visits + 1
-    
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
     context = {
-        'num_books': num_books,
-        'num_instances': num_instances,
-        'num_instances_available': num_instances_available,
-        'num_authors': num_authors,
-        'num_genres_fiction':num_genres_fiction,
-        'num_books_thriller':num_books_thriller,
-        'num_visits': num_visits,  
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "num_authors": num_authors,
+        "num_genres_fiction": num_genres_fiction,
+        "num_books_thriller": num_books_thriller,
+        "num_visits": num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html', context=context)
+    return render(request, "index.html", context=context)
 
 
 class BookListView(generic.ListView):
     model = Book
     paginate_by = 4
-    
 
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+
+    model = BookInstance
+    template_name = "catalog/bookinstance_list_borrowed_user.html"
+    paginate_by = 10
+
+    def get_queryset(self):
+        return (
+            BookInstance.objects.filter(borrower=self.request.user)
+            .filter(status__exact="o")
+            .order_by("due_back")
+        )
